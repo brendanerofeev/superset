f78dcd887b27ba1f568410226fa28eb0316f6428
brendanerofeev <98018651+brendanerofeev@users.noreply.github.com>
2024-12-17 12:55:01 +1000

    Update superset_config.py

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -1,96 +1,119 @@
-# Superset specific config
-ROW_LIMIT = 5000
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# This file is included in the final Docker image and SHOULD be overridden when
+# deploying the image to prod. Settings configured here are intended for use in local
+# development environments. Also note that superset_config_docker.py is imported
+# as a final step as a means to override "defaults" configured here
+#
+import logging
+import os
 
-# Flask App Builder configuration
-# Your App secret key will be used for securely signing the session cookie
-# and encrypting sensitive information on the database
-# Make sure you are changing this key for your deployment with a strong key.
-# Alternatively you can set it with `SUPERSET_SECRET_KEY` environment variable.
-# You MUST set this for production environments or the server will not refuse
-# to start and you will see an error in the logs accordingly.
-SECRET_KEY = '9HugbmFrCPGm+Rh6sNHopn4b00dTcrQjb3WciqCIQt8cgMOPtkHODi8l'
-# SECRET_KEY = 'QpH3S9WZLS+WbguIl0xiGFJNGqJ5qFIEti+NOv33IKjqLwL9mCh/ttYm'
-# The SQLAlchemy connection string to your database backend
-# This connection defines the path to the database that stores your
-# superset metadata (slices, connections, tables, dashboards, ...).
-# Note that the connection information to connect to the datasources
-# you want to explore are managed directly in the web UI
-# The check_same_thread=false property ensures the sqlite client does not attempt
-# to enforce single-threaded access, which may be problematic in some edge cases
-# SQLALCHEMY_DATABASE_URI = 'mysql://keshav.arora:W!nterT1me@fe-mean.cp4lje53dvkz.ap-south-1.rds.amazonaws.com/BI'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:bJn0TyYc4Jn54Bhu7dkt@od-reports.c9omk2ykqhw3.ap-southeast-2.rds.amazonaws.com:5432/superset_db'
-# SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/superset_db'
+from celery.schedules import crontab
+from flask_caching.backends.filesystemcache import FileSystemCache
 
-TALISMAN_ENABLED = False
-WTF_CSRF_ENABLED = False
+logger = logging.getLogger()
 
-# Set this API key to enable Mapbox visualizations
-MAPBOX_API_KEY = ''
+DATABASE_DIALECT = os.getenv("DATABASE_DIALECT")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_DB = os.getenv("DATABASE_DB")
 
-SESSION_COOKIE_SAMESITE = None
-ENABLE_PROXY_FIX = True
-PUBLIC_ROLE_LIKE_GAMMA = True
-FEATURE_FLAGS = {
-    'DASHBOARD_CROSS_FILTERS': True,
-    'HORIZONTAL_FILTER_BAR': True,
-    'ENABLE_TEMPLATE_PROCESSING': True,
-    'EMBEDDED_SUPERSET': True,
+EXAMPLES_USER = os.getenv("EXAMPLES_USER")
+EXAMPLES_PASSWORD = os.getenv("EXAMPLES_PASSWORD")
+EXAMPLES_HOST = os.getenv("EXAMPLES_HOST")
+EXAMPLES_PORT = os.getenv("EXAMPLES_PORT")
+EXAMPLES_DB = os.getenv("EXAMPLES_DB")
+
+# The SQLAlchemy connection string.
+SQLALCHEMY_DATABASE_URI = (
+    f"{DATABASE_DIALECT}://"
+    f"{DATABASE_USER}:{DATABASE_PASSWORD}@"
+    f"{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}"
+)
+
+SQLALCHEMY_EXAMPLES_URI = (
+    f"{DATABASE_DIALECT}://"
+    f"{EXAMPLES_USER}:{EXAMPLES_PASSWORD}@"
+    f"{EXAMPLES_HOST}:{EXAMPLES_PORT}/{EXAMPLES_DB}"
+)
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+REDIS_CELERY_DB = os.getenv("REDIS_CELERY_DB", "0")
+REDIS_RESULTS_DB = os.getenv("REDIS_RESULTS_DB", "1")
+
+RESULTS_BACKEND = FileSystemCache("/app/superset_home/sqllab")
+
+CACHE_CONFIG = {
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_",
+    "CACHE_REDIS_HOST": REDIS_HOST,
+    "CACHE_REDIS_PORT": REDIS_PORT,
+    "CACHE_REDIS_DB": REDIS_RESULTS_DB,
 }
-
-HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
-ENABLE_CORS = True
-
-CORS_OPTIONS = {
-  'supports_credentials': True,
-  'allow_headers': ['*'],
-  'resources':['*'],
-  'origins': ['*']
-}
+DATA_CACHE_CONFIG = CACHE_CONFIG
 
 
-#GUEST_ROLE_NAME = "Gamma"
-#GUEST_TOKEN_JWT_SECRET = "9HugbmFrCPGm+Rh6sNHopn4b00dTcrQjb3WciqCIQt8cgMOPtkHODi8l"
-#GUEST_TOKEN_JWT_ALGO = "HS256"
-#GUEST_TOKEN_HEADER_NAME = "X-GuestToken"
-#GUEST_TOKEN_JWT_EXP_SECONDS = 300  # 5 minutes
+class CeleryConfig:
+    broker_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}"
+    imports = (
+        "superset.sql_lab",
+        "superset.tasks.scheduler",
+        "superset.tasks.thumbnails",
+        "superset.tasks.cache",
+    )
+    result_backend = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}"
+    worker_prefetch_multiplier = 1
+    task_acks_late = False
+    beat_schedule = {
+        "reports.scheduler": {
+            "task": "reports.scheduler",
+            "schedule": crontab(minute="*", hour="*"),
+        },
+        "reports.prune_log": {
+            "task": "reports.prune_log",
+            "schedule": crontab(minute=10, hour=0),
+        },
+    }
 
-#EXTRA_CATEGORICAL_COLOR_SCHEMES['od_color_scheme'] = ['#AECCC5', '#373938', '#DCD3C2', '#4A9A6B', '#F4806A']
 
+CELERY_CONFIG = CeleryConfig
 
-EXTRA_CATEGORICAL_COLOR_SCHEMES = [
-    {
-        "id": 'odColors',
-        "description": '',
-        "label": 'OD Colors',
-        "isDefault": False,
-        "colors":
-         ['#AECCC5', '#373938', '#DCD3C2', '#4A9A6B', '#F4806A']
-    }]
+FEATURE_FLAGS = {"ALERT_REPORTS": True}
+ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
+WEBDRIVER_BASEURL = "http://superset:8088/"  # When using docker compose baseurl should be http://superset_app:8088/
+# The base URL for the email report hyperlinks.
+WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
+SQLLAB_CTAS_NO_LIMIT = True
 
+#
+# Optionally import superset_config_docker.py (which will have been included on
+# the PYTHONPATH) in order to allow for local settings to be overridden
+#
+try:
+    import superset_config_docker
+    from superset_config_docker import *  # noqa
 
-# THEME_OVERRIDES = {
-#   "borderRadius": 4,
-#   "colors": {
-#     "primary": {
-#      "base": 'red',
-#     },
-#     "secondary": {
-#       "base": 'green',
-#     },
-#     "grayscale": {
-#       "base": 'orange',
-#     }
-#   }
-# } 
-
-# EXTRA_SEQUENTIAL_COLOR_SCHEMES =  [
-#     {
-#         "id": 'warmToHot',
-#         "description": '',
-#         "isDiverging": True,
-#         "label": 'My custom warm to hot',
-#         "isDefault": True,
-#         "colors":
-#          ['#552288', '#5AAA46', '#CC7788', '#EEDD55', '#9977BB', '#BBAA44', '#DDCCDD',
-#          '#006699', '#009DD9', '#5AAA46', '#44AAAA', '#DDAA77', '#7799BB', '#88AA77']
-#     }]
+    logger.info(
+        f"Loaded your Docker configuration at " f"[{superset_config_docker.__file__}]"
+    )
+except ImportError:
+    logger.info("Using default Docker config...")
